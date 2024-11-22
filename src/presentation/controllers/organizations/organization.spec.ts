77ece1be73867b611d13397a2d1ed20e648b301c
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-11-21 21:14:44 -0300

    feat: ensure OrganizationController return 400 if no nick is provided

--- src/presentation/controllers/organizations/organization.spec.ts
+++ src/presentation/controllers/organizations/organization.spec.ts
@@ -22,5 +22,22 @@ describe("Organization Controller", () => {
 
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+  });
+
+  test("Should return 400 if no nick is provided", async () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        age: 16,
+        nationality: "any_nationality",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: nick"));
   });
 });
--- src/presentation/controllers/organizations/organization.ts
+++ src/presentation/controllers/organizations/organization.ts
@@ -2,9 +2,15 @@ import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 export class OrganizationController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return {
-      statusCode: 400,
-      body: {},
-    };
+    const requiredFields = ["name", "nick", "age", "nationality"];
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing param: ${field}`),
+        };
+      }
+    }
   }
 }
