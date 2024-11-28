5b3c25fda39ab9ad07fe3968fcb7eac4d63c6af5
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-11-27 23:22:43 -0300

    feat: ensure ChampionshipController return 400 if no championshipName is provided

--- src/presentation/controllers/championship/championship.spec.ts
+++ src/presentation/controllers/championship/championship.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from "../../errors/missing-param-error";
 import { success } from "../../helpers/http/http-helper";
 import { HttpRequest } from "../../protocols";
 import { ChampionshipController } from "./championship";
@@ -16,6 +17,22 @@ const makeSut = () => {
 };
 
 describe("Championship Controller", () => {
+  test("Should return 400 if no championshipName is provided", async () => {
+    const sut = makeSut();
+
+    const httpRequest = {
+      body: {
+        award: 10000.0,
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError("championshipName")
+    );
+  });
+
   test("Should return 200 if valid data is provided", async () => {
     const sut = makeSut();
     const httpResponse = await sut.handle(makeFakeRequest());
